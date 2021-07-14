d0741437a0965c235c0f33cfc71d156ffdfbcad5
Nami <zkdn4067@gmail.com>
2021-07-14 23:42:05 +0900

    signin component

--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -1,42 +1,62 @@
+import { useRef } from "react";
 import { Row, Col, Input, Button } from 'antd';
 import styles from './Signin.module.css';
 
-export default function Signin() {
+// interface SigninProps {}
+
+
+const Signin: React.FC<SigninProps> = () =>{
+    const emailRef = useRef<Input>(null);
+    const passwordRef = useRef<Input>(null);
+
     return (
         <Row align="middle" className={styles.signin_row}>
             <Col span={24}>
                 <Row className={styles.signin_contents}>
-                    <Col span={12}></Col>
                     <Col span={12}>
-                        <div>my books</div>
-                        <div>Please Note your Opinion</div>
+                        <img src="/bg_signin.png" alt="signin" className={ styles.signin_bg}/>
+                    </Col>
+                    <Col span={12}>
+                        <div className={styles.signin_title}>my books</div>
+                        <div className={styles.signin_subtitle}>Please Note your Opinion</div>
                         <div />
-                        <div>Email
-                            <span>*</span>
+                        <div className={styles.signin_underline}>
                         </div>
-                        <div>
+                        <div className={styles.input_area}>
+                            Email
+                            <span className={styles.required}>*</span>
                             <Input
                                 placeholder="Email"
                                 autoComplete="email"
                                 name="email"
+                                className={styles.input}
+                                ref={emailRef}
                             />
                         </div>
-                        <div>Password
-                            <span>*</span>
+                        <div className={styles.password_title}>Password
+                            <span className={styles.required}>*</span>
                         </div>
-                        <div>
+                        <div className={styles.input_area}>
                             <Input
                                 type="password"
                                 autoComplete="current-password"
                                 name="password"
+                                className={styles.input}
+                                ref={passwordRef}
                             />
                         </div>
-                        <div>
-                            <Button size="large">Sign in</Button>
+                        <div className={styles.button_area}>
+                            <Button size="large" className={styles.button}>Sign in</Button>
                         </div>
                     </Col>
                 </Row>
             </Col>
         </Row>
     )
+
+    function click() {
+        const email = emailRef.current!.state.value;
+        const password= passwordRef.current!.state.value;
+        
+    }
 }
--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SigninContainer from '../containers/signinContainer';
 
+
 export default function Signin() {
     return (
         <div>
